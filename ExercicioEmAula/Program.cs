0a2e16dc751ceb735535ff834dd9abe170d28f81
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-22 13:25:18 -0300

    Add project files.

---
+++ ExercicioEmAula/Program.cs
@@ -0,0 +1,20 @@
+﻿int valor;
+bool repete;
+
+do
+{
+    Console.WriteLine("Informe um valor inteiro...");
+    try
+    {
+        valor = int.Parse(Console.ReadLine());
+        repete = false;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Por favor, informe um valor inteiro\n" +
+            $"{e.Message}");
+        repete = true;
+        Console.Clear();
+    }    
+}
+while (repete);
